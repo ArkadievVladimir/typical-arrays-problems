e77284e999ce5adb3a1b85de2feb6b8930e5e1e7
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-09-30 02:56:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = [0]) {
+    array.map(elem => {if (elem < array[0]) array[0] = elem});
+    return array.length == 0 ? 0 : array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = [0]) {
+    array.map(elem => {if (elem > array[0]) array[0] = elem});
+    return array.length == 0 ? 0 : array[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = [0]) {
+    let sum = 0;
+    array.map(elem => sum += elem);
+    return array.length == 0 ? 0 : sum/array.length;
 }
